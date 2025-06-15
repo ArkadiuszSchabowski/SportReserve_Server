403b70c90b9d04b26e01b26f1b7dddd6f4b5f530
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-06-15 17:42:50 +0200

    feat: add REST endpoints to AccountController for user management

--- Server/SportReserveServer/Controllers/AccountController.cs
+++ Server/SportReserveServer/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using SportReserveServer.Interfaces.Aggregates;
+using SportReserveServer.Models;
 
 namespace SportReserveServer.Controllers
 {
@@ -7,5 +8,53 @@ namespace SportReserveServer.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly IAccountAggregateService _service;
+
+        public AccountController(IAccountAggregateService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<List<GetUserDto>> Get()
+        {
+            return _service.Get();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<GetUserDto> Get([FromRoute] int id)
+        {
+            return _service.Get(id);
+        }
+
+        [HttpGet("email")]
+        public ActionResult<GetUserDto> Get([FromQuery] string email)
+        {
+            return _service.Get(email);
+        }
+
+        [HttpPost]
+        public ActionResult Add([FromBody] AddUserDto dto)
+        {
+            _service.Add(dto);
+
+            return Ok();
+        }
+
+        [HttpPost("login")]
+        public ActionResult<string> Login([FromBody] LoginDto dto)
+        {
+            var token = _service.GenerateJwt(dto);
+
+            return Ok(token);
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Remove([FromRoute] int id)
+        {
+            _service.Remove(id);
+
+            return NoContent();
+        }
     }
 }
