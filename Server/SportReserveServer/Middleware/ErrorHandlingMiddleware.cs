d58940cf6f6151c690ee25c48c83d685257668e9
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-06-15 17:43:22 +0200

    feat: create error handling middleware

---
+++ Server/SportReserveServer/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿using SportReserveServer.Exceptions;
+
+namespace SportReserveServer.Middleware
+{
+    public class ErrorHandlingMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next.Invoke(context);
+            }
+            catch (BadRequestException e)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(e.Message);
+            }
+
+            catch (NotFoundException e)
+            {
+                context.Response.StatusCode = 404;
+                await context.Response.WriteAsync(e.Message);
+            }
+            catch (ConflictException e)
+            {
+                context.Response.StatusCode = 409;
+                await context.Response.WriteAsync(e.Message);
+            }
+            catch (Exception)
+            {
+                context.Response.StatusCode = 500;
+                await context.Response.WriteAsync("Błąd serwera!");
+            }
+        }
+    }
+}
