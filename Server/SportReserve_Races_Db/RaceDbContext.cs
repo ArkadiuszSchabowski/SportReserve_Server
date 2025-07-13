ec1f351c31ca1c80e04e25e770544656a89fca3b
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-13 19:44:22 +0200

    feat: create database project for races

---
+++ Server/SportReserve_Races_Db/RaceDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportReserve_Races_Db.Entities;
+
+namespace SportReserve_Races_Db
+{
+    public class RaceDbContext : DbContext
+    {
+        public DbSet<Race> Races { get; set; }
+        public RaceDbContext(DbContextOptions<RaceDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
