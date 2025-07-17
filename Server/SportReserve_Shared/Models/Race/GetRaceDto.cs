c1258ef993eefb7f788c6281399408c0f63270b1
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-17 17:20:19 +0200

    feat: add get race dto

--- Server/SportReserve_Shared/Models/Race/GetRaceDto.cs
+++ Server/SportReserve_Shared/Models/Race/GetRaceDto.cs
@@ -2,5 +2,13 @@
 {
     public class GetRaceDto
     {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public DateOnly DateOfStart { get; set; }
+        public string City { get; set; } = string.Empty;
+        public string Place { get; set; } = string.Empty;
+        public double Distance { get; set; }
+        public int Slots { get; set; }
+        public bool IsRegistrationOpen { get; set; }
     }
 }
