be23f8ef70f00ee94f189543f6b91bd83d18f41d
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-17 17:27:35 +0200

    refactor: implement IValidatorInput for consistency

--- Server/SportReserve_Users/Interfaces/Aggregates/IUserAggregateValidator.cs
+++ Server/SportReserve_Users/Interfaces/Aggregates/IUserAggregateValidator.cs
@@ -5,7 +5,7 @@ using SportReserve_Users_Db.Entities;
 
 namespace SportReserve_Users.Interfaces.Aggregates
 {
-    public interface IUserAggregateValidator : IEntityValidator<User>, IUserValidator, ILoginValidator, IValidatorId, IEmailValidator
+    public interface IUserAggregateValidator : IEntityValidator<User>, IValidatorInput<RegisterDto>, IUserValidator, ILoginValidator, IValidatorId, IEmailValidator
     {
     }
 }
