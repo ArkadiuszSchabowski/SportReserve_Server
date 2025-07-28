6a460c6831bf141620413599ede986c4dca200f6
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-28 19:14:35 +0200

    feat: add configuration and binding for emailAuthentication, add middleware

---
+++ Server/SportReserve_Emails/EmailAuthentication.cs
@@ -0,0 +1,8 @@
+﻿namespace SportReserve_Emails
+{
+    public class EmailAuthentication
+    {
+        public string Email { get; set; } = string.Empty;
+        public string AppPassword { get; set; } = string.Empty;
+    }
+}
