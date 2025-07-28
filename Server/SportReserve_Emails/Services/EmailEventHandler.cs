5300a5b8b3d7fae1fd47f0c45dcfd88a4652c552
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-28 19:12:29 +0200

    feat: create EmailEventHandler with IEmailEventHandler

--- Server/SportReserve_Emails/Services/EmailEventHandler.cs
+++ Server/SportReserve_Emails/Services/EmailEventHandler.cs
@@ -1,15 +1,39 @@
-﻿using SportReserve_Emails.Interfaces;
+﻿using FluentEmail.Core;
+using FluentEmail.Smtp;
+using SportReserve_Emails.Interfaces;
 using SportReserve_Shared.Models.User;
+using System.Net;
+using System.Net.Mail;
 
 namespace SportReserve_Emails.Services
 {
     public class EmailEventHandler : IEmailEventHandler
     {
-        public void SendRegisterEmail(UserRegisteredEventDto dto)
+        private readonly EmailAuthentication _emailAuthentication;
+
+        public EmailEventHandler(EmailAuthentication emailAuthentication)
         {
-            //TO DO
-            var task = "";
-            var nekw = "jdjdj";
+            _emailAuthentication = emailAuthentication;
+        }
+        public async Task SendRegisterEmail(UserRegisteredEventDto dto)
+        {
+                var sender = new SmtpSender(() => new SmtpClient("smtp.gmail.com")
+                {
+                    EnableSsl = true,
+                    Port = 587,
+                    Credentials = new NetworkCredential(_emailAuthentication.Email, _emailAuthentication.AppPassword)
+                });
+
+                Email.DefaultSender = sender;
+
+                var email = await Email
+                    .From(emailAddress: _emailAuthentication.Email)
+                    .To(emailAddress: dto.Email)
+                    .Subject("Registration")
+                    .Body($"Welcome to our app, {dto.Name}! Your adventure starts today with us. You can reserve runs that interest you and track your progress. See you on the race track!")
+                    .SendAsync();
         }
     }
 }
+
+
