23200d1b7817f1034370c2c19ab29c1fd3ec8b72
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-07-28 19:23:34 +0200

    feat: add ValidateRegisterStepOne contract

---
+++ Server/SportReserve_Users/Interfaces/IValidateRegisterStepOne.cs
@@ -0,0 +1,9 @@
+﻿using SportReserve_Shared.Models.User;
+
+namespace SportReserve_Users.Interfaces
+{
+    public interface IValidateRegisterStepOne
+    {
+        Task ValidateRegisterStepOne(RegisterStepOneDto dto);
+    }
+}
