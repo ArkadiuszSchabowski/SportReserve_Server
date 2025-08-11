f0c656aaf9f8a9c297335a10105783f3b58f1569
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-11 21:02:09 +0200

    refactor: move entry fee GBP and Date of Start to Race

--- Server/SportReserve_Races_Db/Entities/Race.cs
+++ Server/SportReserve_Races_Db/Entities/Race.cs
@@ -4,7 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
+        public DateOnly DateOfStart { get; set; }
+        public string Description { get; set; } = string.Empty;
         public string? PosterUrl { get; set; }
+        public double? EntryFeeGBP { get; set; }
         public List<RaceTrace> RaceTraces { get; set; } = new();
     }
 }
