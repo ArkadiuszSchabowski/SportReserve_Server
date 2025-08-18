84047dd9f27c175134e7b41929cd89535db78f37
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-08-18 18:51:41 +0200

    migration: add default poster url

---
+++ Server/SportReserve_Races_Db/Migrations/20250817155640_AddDefaultPosterUrlValue.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SportReserve_Races_Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDefaultPosterUrlValue : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PosterUrl",
+                table: "Races",
+                type: "nvarchar(max)",
+                nullable: true,
+                defaultValue: "images/default_poster.png",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PosterUrl",
+                table: "Races",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true,
+                oldDefaultValue: "images/default_poster.png");
+        }
+    }
+}
