49bd818bb92c66c8c8202ebb9d98d1fa5c0e24e9
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-12 21:20:39 +0200

    feat: update program.cs

--- Server/SportReserve_Reservations/Program.cs
+++ Server/SportReserve_Reservations/Program.cs
@@ -1,5 +1,14 @@
+using Microsoft.IdentityModel.Tokens;
+using SportReserve_ApiGateway.Helpers;
+using SportReserve_ApiGateway.Validators;
+using SportReserve_Reservations;
 using SportReserve_Reservations.DataAccess;
 using SportReserve_Reservations.Interfaces;
+using SportReserve_Reservations.Services;
+using SportReserve_Shared.Interfaces;
+using SportReserve_Shared.Middleware;
+using System.Text;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,9 +16,61 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        options.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    });
+
+var authenticationSettings = new AuthenticationSettings();
+
+builder.Services.AddSingleton(authenticationSettings);
+builder.Configuration.GetSection("Authentication").Bind(authenticationSettings);
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = "Bearer";
+    option.DefaultScheme = "Bearer";
+    option.DefaultChallengeScheme = "Bearer";
+}).AddJwtBearer(cfg =>
+{
+    cfg.RequireHttpsMetadata = false;
+    cfg.SaveToken = true;
+    cfg.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = authenticationSettings.JwtIssuer,
+        ValidAudience = authenticationSettings.JwtIssuer,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationSettings.JwtKey)),
+    };
+});
+
 builder.Services.AddScoped<IReservationAccess, ReservationAccess>();
+builder.Services.AddScoped<IReservationService, ReservationService>();
+
+builder.Services.AddScoped<IHttpResponseValidator, HttpResponseValidator>();
+builder.Services.AddScoped<IHttpResponseHelper, HttpResponseHelper>();
+
+builder.Services.AddHttpClient("UserService", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5001/api/user/");
+});
+
+builder.Services.AddHttpClient("RaceService", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5002/api/race/");
+});
+
+builder.Services.AddHttpClient("RaceTraceService", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5002/api/racetrace/");
+});
+
+builder.Services.AddScoped<ErrorHandlingMiddleware>();
 
 var app = builder.Build();
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
 {
