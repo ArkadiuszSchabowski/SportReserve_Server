0866cb40b6c1de5471554b8955c91e641e715b86
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-12 21:17:27 +0200

    feat: create AnimalShelterRace, LondonHalfMarathonRace and ValentineRace models

---
+++ Server/SportReserve_Shared/Models/Reservation/LondonHalfMarathonRace.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SportReserve_Shared.Models.Reservation
+{
+    public class LondonHalfMarathonRace
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public int RaceId { get; set; }
+        public int RaceTraceId { get; set; }
+        public string TShirtSize { get; set; } = string.Empty;
+
+        public string? TeamName;
+        public bool MedalGratification { get; set; } = false;
+    }
+}
---
+++ Server/SportReserve_Shared/Models/Reservation/ValentineRace.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace SportReserve_Shared.Models.Reservation
+{
+    public class ValentineRace
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+        public int UserId { get; set; }
+        public int RaceId { get; set; }
+        public int RaceTraceId { get; set; }
+        public string ValentineGadget { get; set; } = string.Empty;
+
+        public string RunType = string.Empty;
+        public bool WantsFinisherPhoto { get; set; } = false;
+    }
+}
