bd58ff835ebba256b335181f480ed620dd0a92d0
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-12 21:19:50 +0200

    feat: AddAnimalShelterRace endpoint

---
+++ Server/SportReserve_Reservations/Interfaces/IReservationService.cs
@@ -0,0 +1,9 @@
+﻿using SportReserve_Shared.Models.Reservation;
+
+namespace SportReserve_Reservations.Interfaces
+{
+    public interface IReservationService
+    {
+        Task AddReservation(AnimalShelterRace race);
+    }
+}
