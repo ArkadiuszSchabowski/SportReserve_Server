fd8b45c16a78a9d0de04cfba5facd8d350869f2a
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-13 20:42:41 +0200

    refactor: remove unused dependencies and authorizationHeader

--- Server/SportReserve_Reservations/Controllers/ReservationController.cs
+++ Server/SportReserve_Reservations/Controllers/ReservationController.cs
@@ -1,11 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 using SportReserve_Reservations.Interfaces;
-using SportReserve_Shared.Interfaces;
 using SportReserve_Shared.Models.Reservation;
 using System.Security.Claims;
-using System.Text.Json;
 
 namespace SportReserve_Reservations.Controllers
 {
@@ -14,23 +11,15 @@ namespace SportReserve_Reservations.Controllers
     public class ReservationController : ControllerBase
     {
         private readonly IReservationService<AnimalShelterRace> _reservationService;
-        private readonly IHttpClientFactory _httpClientFactory;
-        private readonly IHttpResponseHelper _httpResponseHelper;
-        private readonly IHttpResponseValidator _httpResponseValidator;
-        private readonly JsonSerializerOptions _jsonOptions;
 
-        public ReservationController(IReservationService<AnimalShelterRace> reservationService, IHttpClientFactory httpClientFactory, IHttpResponseValidator httpResponseValidator, IHttpResponseHelper httpResponseHelper, IOptions<JsonOptions> jsonOptions)
+        public ReservationController(IReservationService<AnimalShelterRace> reservationService)
         {
             _reservationService = reservationService;
-            _httpClientFactory = httpClientFactory;
-            _httpResponseHelper = httpResponseHelper;
-            _httpResponseValidator = httpResponseValidator;
-            _jsonOptions = jsonOptions.Value.JsonSerializerOptions;
         }
 
         [Authorize]
         [HttpPost("animal-shelter-race")]
-        public async Task<IActionResult> Add([FromHeader(Name = "Authorization")] string? authorizationHeader, AnimalShelterRace reservation)
+        public async Task<IActionResult> Add(AnimalShelterRace reservation)
         {
             var userIdFromToken = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
