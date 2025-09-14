10620898086c37be97db7fde8a359ac2771c00dc
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-14 19:43:50 +0200

    feat: update models for more friendly UI

--- Server/SportReserve_Shared/Models/Reservation/AnimalShelterRace.cs
+++ Server/SportReserve_Shared/Models/Reservation/AnimalShelterRace.cs
@@ -6,6 +6,11 @@ namespace SportReserve_Shared.Models.Reservation
     [BsonDiscriminator("AnimalShelterRace")]
     public class AnimalShelterRace : ReservationBase
     {
+        public string RaceName { get; set; } = string.Empty;
+        public string Location { get; set; } = string.Empty;
+        public DateOnly DateOfStart { get; set; }
+        public TimeOnly HourOfStart { get; set; }
+        public double DistanceKm { get; set; }
         public string? DogSize { get; set; }
         public int? DonationAmount { get; set; }
         public string EmergencyContact { get; set; } = string.Empty;
