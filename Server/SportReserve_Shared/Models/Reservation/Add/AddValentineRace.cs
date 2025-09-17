b952017909127f9c16f3d48a6245952e056513f8
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-17 22:38:33 +0200

    feat: add race models

--- Server/SportReserve_Shared/Models/Reservation/Add/AddValentineRace.cs
+++ Server/SportReserve_Shared/Models/Reservation/Add/AddValentineRace.cs
@@ -1,12 +1,16 @@
 ﻿using SportReserve_Shared.Models.Reservation.Base;
+using System.ComponentModel.DataAnnotations;
 
 namespace SportReserve_Shared.Models.Reservation.Add
 {
     public class AddValentineRace : ReservationBase
     {
+        [Required]
         public string ValentineGadget { get; set; } = string.Empty;
 
+        [Required]
         public string RunType = string.Empty;
+
         public bool WantsFinisherPhoto { get; set; } = false;
     }
 }
