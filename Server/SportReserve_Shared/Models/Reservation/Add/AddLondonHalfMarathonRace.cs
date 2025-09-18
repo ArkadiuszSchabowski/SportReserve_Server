740a1f199dc41077b989ad4fd96129c758a2d515
ArkadiuszSchabowski <arkadiuszschabowski@gmail.com>
2025-09-18 12:41:30 +0200

    fix: set TShirtSize as nullable

--- Server/SportReserve_Shared/Models/Reservation/Add/AddLondonHalfMarathonRace.cs
+++ Server/SportReserve_Shared/Models/Reservation/Add/AddLondonHalfMarathonRace.cs
@@ -1,12 +1,10 @@
 ﻿using SportReserve_Shared.Models.Reservation.Base;
-using System.ComponentModel.DataAnnotations;
 
 namespace SportReserve_Shared.Models.Reservation.Add
 {
     public class AddLondonHalfMarathonRace : ReservationBase
     {
-        [Required]
-        public string TShirtSize { get; set; } = string.Empty;
+        public string? TShirtSize { get; set; }
 
         public string? TeamName;
         public bool MedalGratification { get; set; } = false;
